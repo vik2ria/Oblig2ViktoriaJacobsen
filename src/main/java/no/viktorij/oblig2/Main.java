fc40e446bc5e8d2af74fb28f78f6961eca378f71
vik2ria <77890073+vik2ria@users.noreply.github.com>
2021-10-04 11:06:28 +0200

    Change junit dependencies to include junit5 Add Main class and test class

---
+++ src/main/java/no/viktorij/oblig2/Main.java
@@ -0,0 +1,9 @@
+package no.viktorij.oblig2;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args)
+    {
+
+    }
+}
---
+++ src/test/java/leap_year_test.java
@@ -0,0 +1,10 @@
+import no.viktorij.oblig2.*;
+import org.junit.jupiter.api.Test;
+
+public class leap_year_test {
+
+    @Test()
+    public void is_the_year_a_leap_year() {
+
+    }
+}
