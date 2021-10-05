7cbb7c7e33906558ae21429045ae62fb35012698
vik2ria <77890073+vik2ria@users.noreply.github.com>
2021-10-05 20:58:50 +0200

    Add user input handle

--- src/main/java/no/viktorij/oblig2/Main.java
+++ src/main/java/no/viktorij/oblig2/Main.java
@@ -1,9 +1,17 @@
 package no.viktorij.oblig2;
 import java.io.*;
+import java.sql.SQLOutput;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args)
     {
+        System.out.println("Hi this is a program that tell you if a year \nis a leap year or not.");
+        Scanner userInput = new Scanner(System.in);
+        System.out.println("Enter the year you want to check:");
+        int usersYearInput = userInput.nextInt();
+
 
     }
+
 }
