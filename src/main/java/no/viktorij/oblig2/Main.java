0093e227a3ac20e26dc13dc0d89f7e650279cf3f
vik2ria <77890073+vik2ria@users.noreply.github.com>
2021-10-08 12:12:08 +0200

    add if else statement

--- src/main/java/no/viktorij/oblig2/Main.java
+++ src/main/java/no/viktorij/oblig2/Main.java
@@ -1,17 +1,23 @@
 package no.viktorij.oblig2;
-import java.io.*;
-import java.sql.SQLOutput;
+
 import java.util.Scanner;
+import static no.viktorij.oblig2.LeapYear.isLeapYear;
 
 public class Main {
     public static void main(String[] args)
     {
+        String yearIsLeapYearMessage = "The chosen year is a leap year!";
+        String yearIsNotLeapYearMessage = "The year is not a leap year!";
         System.out.println("Hi this is a program that tell you if a year \nis a leap year or not.");
         Scanner userInput = new Scanner(System.in);
         System.out.println("Enter the year you want to check:");
         int usersYearInput = userInput.nextInt();
 
-
+        if (isLeapYear(usersYearInput)) {
+            System.out.println(yearIsLeapYearMessage);
+        }
+        else
+            System.out.println(yearIsNotLeapYearMessage);
     }
 
 }
