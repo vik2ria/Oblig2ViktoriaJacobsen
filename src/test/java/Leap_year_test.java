98d2a8c491f64092ac56acabb378ae8194d46ca1
vik2ria <77890073+vik2ria@users.noreply.github.com>
2021-10-08 12:11:22 +0200

    Change test to test for one expression

--- src/test/java/Leap_year_test.java
+++ src/test/java/Leap_year_test.java
@@ -11,18 +11,59 @@ import static org.junit.jupiter.api.Assertions.fail;
 
 public class Leap_year_test {
 
+    /* this test checks one of the criteria
+     for a year not to be a leap year. The wanted result is false*/
     @ParameterizedTest
     @MethodSource("year")
-    public void is_the_year_a_leap_year(String expected, int input) {
+    public void is_year_divisible_by_100_and_not_400(boolean expected, int input) {
         assertEquals(expected,
                 LeapYear.isLeapYear(input));
     }
 
+    // use a stream to check multiple input values
     public static Stream year() {
         return Stream.of(
-                Arguments.of("The chosen year is a leap year", 2000),
-                Arguments.of("The chosen year is a leap year", 1972),
-                Arguments.of("The year is not a leap year", 1900)
+                Arguments.of(false, 1700),
+                Arguments.of(false, 1900),
+                Arguments.of(true, 2000),
+                Arguments.of(false, 2100)
+
+        );
+    }
+
+    // check if the year is divisible by 400
+    @ParameterizedTest
+    @MethodSource("year_divide_400")
+    public void is_year_divisible_by_400(boolean expected, int input) {
+        assertEquals(expected,
+                LeapYear.isLeapYear(input));
+    }
+
+    public static Stream year_divide_400() {
+        return Stream.of(
+                Arguments.of(true, 1600),
+                Arguments.of(false, 1900),
+                Arguments.of(true, 2000),
+                Arguments.of(false, 2100)
+
+        );
+    }
+
+    //check if the year is divisible by 4
+    @ParameterizedTest
+    @MethodSource("year_divide_4")
+    public void is_the_year_divisible_4(boolean expected, int input) {
+        assertEquals(expected,
+                LeapYear.isLeapYear(input));
+    }
+
+    public static Stream year_divide_4() {
+        return Stream.of(
+                Arguments.of(true, 1972),
+                Arguments.of(false, 1999),
+                Arguments.of(true, 2000),
+                Arguments.of(false, 2102)
+
         );
     }
 }
